ae998ef99e361207ee766664b1b5028e659a5329
Mat Jordan <mat@utk.edu>
2019-10-02 13:14:59 -0400

    Render universal header.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import Digital from './Digital';
 
 import './scss/styles.scss';
 
-if (document.getElementById('utk-lib-header')) {
-    ReactDOM.render(<Digital />, document.getElementById('utk-lib-header'));
+if (document.getElementById('utk-lib-digital-front')) {
+    ReactDOM.render(<Digital />, document.getElementById('utk-lib-digital-front'));
 }
