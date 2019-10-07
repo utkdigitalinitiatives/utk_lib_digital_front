57c575904b104fdc114daf8b2784cffc9c1393b2
Mat Jordan <mat@utk.edu>
2019-10-07 09:04:23 -0400

    Add dc search component.

---
+++ src/components/partials/Search.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+
+
+        return (
+            <div className="utk-digital--search">
+                <div className="container">
+                    search stuff
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Search;
--- src/components/sections/DigitalHeader.js
+++ src/components/sections/DigitalHeader.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Search from "../partials/Search";
 import Splash from "../partials/Splash";
 
 class DigitalHeader extends Component {
@@ -13,6 +14,7 @@ class DigitalHeader extends Component {
 
         return (
             <section className='utk-digital--header'>
+                <Search/>
                 <Splash collection={featured} />
             </section>
         );
