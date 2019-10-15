136b7fbbb53f6ea8669d4ad497d0fd5c44dd100e
Mat Jordan <mat@utk.edu>
2019-10-15 08:30:01 -0400

    Baseline styling of splash text.

--- src/components/partials/Splash.js
+++ src/components/partials/Splash.js
@@ -15,7 +15,7 @@ class Splash extends Component {
                     <div className="utk-digital--header--splash--featured--content">
                         <h3>Featured Collection</h3>
                         <a href={item.mods_relatedItem_Featured_Item_identifier_pid_s}>
-                            <h4>{item.fgs_label_s}}</h4>
+                            <h4>{item.fgs_label_s}</h4>
                         </a>
                         <a href={item.mods_relatedItem_Featured_Item_identifier_pid_s}>
                             <h5>{item.mods_relatedItem_Featured_Item_titleInfo_title_s}, {item.mods_relatedItem_Featured_Item_dateCreated_s}</h5>
