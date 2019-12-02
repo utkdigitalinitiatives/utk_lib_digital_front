fb64b5495df82b8d63977a88362e39b9fd57432f
Mat Jordan <mat@utk.edu>
2019-12-02 12:33:43 -0500

    Set urls for splash item, update width for search el.

--- src/components/partials/Splash.js
+++ src/components/partials/Splash.js
@@ -9,6 +9,10 @@ class Splash extends Component {
 
     featuredItem = (item) => {
 
+        const baseUrl = 'https://digital.lib.utk.edu/collections/islandora/object/';
+        const collectionUrl = baseUrl + item.PID;
+        const itemUrl = baseUrl + item.mods_relatedItem_Featured_Item_identifier_pid_s;
+
         let title = item.mods_relatedItem_Featured_Item_titleInfo_title_s
 
         if (item.mods_relatedItem_Featured_Item_dateCreated_s)
@@ -19,10 +23,10 @@ class Splash extends Component {
                 <div className="container">
                     <div className="utk-digital--header--splash--featured--content">
                         <h3>Featured Collection</h3>
-                        <a href={item.mods_relatedItem_Featured_Item_identifier_pid_s}>
+                        <a href={collectionUrl}>
                             <h4>{item.fgs_label_s}</h4>
                         </a>
-                        <a href={item.mods_relatedItem_Featured_Item_identifier_pid_s}>
+                        <a href={itemUrl}>
                             <h5>{title}</h5>
                         </a>
                     </div>
